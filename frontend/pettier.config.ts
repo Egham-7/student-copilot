beaaecdb52b12e3baeba88f497b88c796b6c58ae
botirk38 <botir.khaltaev@squaredup.com>
2025-05-02 20:41:09 +0100

    chore(prettier.config): add Prettier configuration file to standardize code formatting across the frontend codebase

---
+++ frontend/pettier.config.ts
@@ -0,0 +1,13 @@
+import type { Options } from 'prettier';
+
+const config: Options = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  tabWidth: 2,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+};
+
+export default config;
